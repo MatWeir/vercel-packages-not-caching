4d80c4da79910a96dcb7eb3a96b3393f89fa5068
Mat Weir <m@tweir.com>
2022-02-02 14:50:08 +1300

    Testing

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Making require('fs') work: https://stackoverflow.com/questions/64926174/module-not-found-cant-resolve-fs-in-next-js-application
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  }
 }
