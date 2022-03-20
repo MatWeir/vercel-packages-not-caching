f411e85b7f5a38585567f7c39ba803f55a3e13a1
Mat Weir <m@tweir.com>
2022-03-20 13:36:36 -0300

    Revert "Revert"
    This reverts commit a8ec78ef1ff0ea1fb1e573ea419056198717c47a.

---
+++ apps/remix-app/remix.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
+module.exports = {
+  appDirectory: 'src',
+  browserBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'api/build',
+};
--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    // Making require('fs') work: https://stackoverflow.com/questions/64926174/module-not-found-cant-resolve-fs-in-next-js-application
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  }
-}
